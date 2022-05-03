e4b4c7c2a14518e6a05dfd4b79f7b8bd22df83a6
Akira Hayashi <smxshxishxad@me.com>
2022-05-03 22:20:05 +0900

    :sparkles: Define `Identifier.TokenLiteral()`

--- ast/ast.go
+++ ast/ast.go
@@ -36,3 +36,11 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
