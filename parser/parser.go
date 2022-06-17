fc1f2072c021ddbd5d87ea852ad7fa026a0deea8
Akira Hayashi <smxshxishxad@me.com>
2022-06-17 18:11:13 +0900

    :sparkles: Add struct `prefixParseFn` and `infixParseFn`

--- parser/parser.go
+++ parser/parser.go
@@ -7,12 +7,20 @@ import (
 	"monkey/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
 	l      *lexer.Lexer
 	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
